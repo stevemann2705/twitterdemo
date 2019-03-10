60ca9bf812d938c5658b32fdbdd6793773c5eb37
Steve Mann <stevemann2705@gmail.com>
2019-03-10 21:24:13 +0530

    Removing unnecessary print statements

--- src/main/java/in/stevemann/twitterdemo/TwitterdemoApplication.java
+++ src/main/java/in/stevemann/twitterdemo/TwitterdemoApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TwitterdemoApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(TwitterdemoApplication.class, args);
     }
