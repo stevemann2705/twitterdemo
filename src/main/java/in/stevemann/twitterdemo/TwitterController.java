c62079c8b87ea23f9a8b27a351cddb94e04e9fa5
Steve Mann <stevemann2705@gmail.com>
2019-04-15 16:05:09 +0530

    Stuck with 140 char limit for a tweet

--- src/main/java/in/stevemann/twitterdemo/TwitterController.java
+++ src/main/java/in/stevemann/twitterdemo/TwitterController.java
@@ -32,12 +32,20 @@ public class TwitterController {
     @RequestMapping("/gettweet/{tweetId}")
     public String getTweet(@PathVariable String tweetId, Model model) {
         Tweet tweet = twitterService.getTweet(twitterTemplateCreator.getTwitterTemplate("stevemann2705"), tweetId);
-//        model.addAttribute("tweetId", tweetId);
-//        model.addAttribute("tweetText", tweet.getUnmodifiedText());
-//        model.addAttribute("tweetUser", tweet.getUser().getName());
-//        model.addAttribute("tweetRetweetCount", tweet.getRetweetCount());
-//        model.addAttribute("tweetFavoriteCount", tweet.getFavoriteCount());
-        return "redirect:" + tweet.getText().substring(tweet.getText().length() - 23);
-//        return "showtweet";
+        model.addAttribute("tweetId", tweetId);
+        /*
+        Spring Social Project is dead.
+        Development stopped before the charachter limit on Twitter was increased from 140 to 280.
+        Hence, the Tweet object expects text of the tweet to be not more than 140.
+        For any tweet with more than 140 charachters, Twitter API now truncates the tweet and adds a link to the tweet at the end.
+        To get the full text of the tweet, add parameter tweet_mode with value extended to your request parameters. (Handled by Spring Social)
+        For more info: https://stackoverflow.com/a/40454382
+         */
+        model.addAttribute("tweetText", tweet.getUnmodifiedText());
+        model.addAttribute("tweetUser", tweet.getUser().getName());
+        model.addAttribute("tweetRetweetCount", tweet.getRetweetCount());
+        model.addAttribute("tweetFavoriteCount", tweet.getFavoriteCount());
+        //return "redirect:" + tweet.getText().substring(tweet.getText().length() - 23);
+        return "showtweet";
     }
 }
